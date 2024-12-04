c315cde59a3ec8424d93e1e7681d04bf65ca6dbf
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 18:26:28 +0000

    correct psyco style

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,6 +7,7 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Return a list of tuples containing each element of the input iterable and its length.
+    Return a list of tuples containing each
+    element of the input iterable and its length.
     """
     return [(i, len(i)) for i in lst]
