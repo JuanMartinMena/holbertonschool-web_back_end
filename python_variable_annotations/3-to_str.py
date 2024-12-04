1eaec9833a7a87c5940c828fb963980a7c3577e8
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:55:02 +0000

    task 3 done

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Pasar un float a un string con validation
+"""
+
+def to_str(n: float) -> str:
+    """
+    transforma un float a un string
+    """
+    return str(n)
