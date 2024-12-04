5a9a6960ecf7f72d68d632416e8059e4cf7ed918
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 17:59:40 +0000

    fix psyco style of task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,6 +8,6 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     funcion que devuelve la tupla con el
-    str k y el cuadrado del int/float v 
+    str k y el cuadrado del int/float v
     """
     return (k, float(v ** 2))
