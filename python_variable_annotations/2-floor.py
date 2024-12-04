26dde0289cfeb8b84d828f539a5d0adceac07112
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:50:13 +0000

    task 2 done

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+"""
+This module defines a type-annotated function to calculate the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Esta función redondea para abajo un float
+    """
+    return math.floor(n)
