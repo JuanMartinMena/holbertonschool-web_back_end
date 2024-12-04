7933343f3a9e805a9b5a26435b59562b53cd3cb6
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 13:00:20 +0000

    task 5 done

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+El objetivo es sumar todos los float de una lista
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    funcion que suma todos los float de una lista y devuelve un float
+    """
+    return sum(input_list)
