eaccd02d850a4a92ace588cebe2722f74e33ae5b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:45:42 +0000

    change function name from "add" to "concat"

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,7 +4,7 @@ El objetio es hacer una funcion con type-anotation que concatene 2 strings
 """
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     Esta función concatena 2 str y devuelve un str
     """
