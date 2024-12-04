0d972965875914b2aa6defe99b3752328444c472
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 17:57:42 +0000

    correct psyco style

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,8 @@
 """
 This module defines a type-annotated function to sum a list containing integers and floats.
 """
-
-
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,9 +2,8 @@
 """
 This module defines a type-annotated function to_kv.
 """
-
-
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
