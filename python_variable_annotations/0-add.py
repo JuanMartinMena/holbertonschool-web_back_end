a8bd0bedf0e3f1aeb929407e236707aa610dc23e
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:31:14 +0000

    module and function commented

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""
+El objetio es hacer una funcion con type-anotation que sume 2 float
+"""
 def add(a: float, b: float) -> float:
+    """
+    Esta función suma 2 float y devuelve un float
+    """
     return a + b
