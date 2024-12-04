0eeab1137f5f00a9e583e7173040600f80eedaa2
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:57:27 +0000

    task 4 done

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+El objetivo es crear variables y asignarles un tipo de dato definido
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
