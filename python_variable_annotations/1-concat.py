f0980673a633b2a2476ce6ccbd005707a8a4a961
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 12:44:26 +0000

    task 1 done

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+El objetio es hacer una funcion con type-anotation que concatene 2 strings
+"""
+
+
+def add(str1: str, str2: str) -> str:
+    """
+    Esta función concatena 2 str y devuelve un str
+    """
+    return str1 + str2
