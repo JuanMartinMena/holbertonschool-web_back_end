896fb5eb8e2c55dff53dcca14e0a696170711b2a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-04 17:56:30 +0000

    task 6 & 7 done

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module defines a type-annotated function to sum a list containing integers and floats.
+"""
+
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Esta funcion suma una lista de int y float
+    """
+    return sum(mxd_lst)
---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module defines a type-annotated function to_kv.
+"""
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    funcion que devuelve la tupla con el str k y el cuadrado del int/float v 
+    """
+    return (k, float(v ** 2))
