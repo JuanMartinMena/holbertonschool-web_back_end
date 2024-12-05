97b62555c55ae6b9bb407e6efb28b92c82981ff4
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-05 15:15:52 +0000

    task3  import fixed

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,7 +4,7 @@ Module for creating asyncio tasks.
 """
 import asyncio
 import importlib
-wait_random = importlib.import_module('python_async_function.0-basic_async_syntax').wait_random
+wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
