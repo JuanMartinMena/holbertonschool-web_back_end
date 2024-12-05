f5712fedd378a57b9feb77a5ad097185873e75c3
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-05 14:19:25 +0000

    task 0 psyco style

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,13 +2,14 @@
 """
 Module for demonstrating basic asynchronous syntax in Python
 """
-
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds
+    Asynchronous coroutine that waits for
+    a random delay between 0 and max_delay seconds
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
