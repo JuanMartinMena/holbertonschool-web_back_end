350ccb2d197b0541c854dfbc9e083630a6425d38
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-05 14:18:15 +0000

    Fixing task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """
-The objetive is to create a function that generates a random wait time
+Module for demonstrating basic asynchronous syntax in Python
 """
+
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay
-    between 0 and max_delay seconds and returns the delay.
+    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds
     """
-    delay = random.uniform(0, max_delay)  # Generate a random float between 0 and max_delay
-    await asyncio.sleep(delay)  # Pause execution for the delay time
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
