f5765e9e39a74516304e83053dbe8076ec21b20d
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-09 13:23:20 +0000

    correct psyco

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Este módulo define un generador asíncrono que produce números aleatorios.
+Este módulo define un generador
+asíncrono que produce números aleatorios.
 """
 import asyncio
 import random
@@ -9,8 +10,10 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Generador asíncrono que produce 10 números aleatorios entre 0 y 10,
-    con una espera asíncrona de 1 segundo entre cada generación.
+    Generador asíncrono que produce
+    10 números aleatorios entre 0 y 10,
+    con una espera asíncrona de
+    1 segundo entre cada generación.
     """
     for _ in range(10):
         await asyncio.sleep(1)
