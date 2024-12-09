ffd2dc1378624ab674df016fb7d73ce1e5e90b44
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-09 14:02:19 +0000

    task 2 done

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+asyncio.gather and time comprehension
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Ejecuta async_comprehension 4 veces en paralelo utilizando asyncio.gather.
+    Mide el tiempo total que tarda en ejecutarse.
+    """
+    start_time = time.time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    
+    end_time = time.time()
+
+    return end_time - start_time
