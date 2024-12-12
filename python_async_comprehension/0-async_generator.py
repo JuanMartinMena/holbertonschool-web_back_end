b349e23f209a52b703669619b7291582cfd1a03a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-12 15:08:19 +0000

    task 0 done

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,20 +1,8 @@
-#!/usr/bin/env python3
-"""
-Este módulo define un generador
-asíncrono que produce números aleatorios.
-"""
-import asyncio
 import random
-from typing import AsyncGenerator
+import time
+from typing import Generator
 
-
-async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Generador asíncrono que produce
-    10 números aleatorios entre 0 y 10,
-    con una espera asíncrona de
-    1 segundo entre cada generación.
-    """
+def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
-        await asyncio.sleep(1)
+        time.sleep(1)
         yield random.uniform(0, 10)
