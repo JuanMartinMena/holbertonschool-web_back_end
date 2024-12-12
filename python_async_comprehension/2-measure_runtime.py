33b32228918bf1de7c46b02d2cb4107d5228c621
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-12 15:03:46 +0000

    correct pyscodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Ejecuta async_comprehension 4 veces en paralelo utilizando asyncio.gather.
