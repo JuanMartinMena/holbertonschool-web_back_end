c820cc06a39d0993dca125e40ee21a61d4b0510c
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-12 11:54:42 +0000

    correct psycostyle 1-simple_pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -9,20 +9,28 @@ import os
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of start and end indexes corresponding to the range of indexes to return in a paginated dataset."""
+    """
+    Return a tuple of start and end indexes corresponding to
+    the range of indexes to return in a paginated dataset.
+    """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
 
+
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """
+    Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = os.path.join(os.path.dirname(__file__), "Popular_Baby_Names.csv")
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """
+        Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,10 +40,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset."""
+        """
+        Return the appropriate page of the dataset.
+        """
         # Validate inputs
-        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
+        assert isinstance(page, int) and page > 0, \
+            "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "page_size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
