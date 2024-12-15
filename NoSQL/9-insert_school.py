b3071abc794e3c890cf44b4c52fa32ab0cb60984
JuanMartinMena <juanmartinmenac@gmail.com>
2024-12-15 15:15:59 +0000

    task 9 done

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Module to insert a new document in a MongoDB collection"""
-
+"""
+Module to insert a new document in a MongoDB collection.
+"""
 
 def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs.
     """
-    if not mongo_collection:
-        return None
+    if mongo_collection is None:
+        raise ValueError("mongo_collection cannot be None")
 
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
